93de66c9bd1e43de0b1133973e74c38eabaf946e
JDVinueza <jdvinueza@outlook.com>
2025-10-27 11:15:56 -0500

    Actualización de estructura y de estilos

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
 import './App.css'
+import { Sections } from './data/Sections'
 
 function App() {
 
   return (
     <>
-      <main className='rounded-xl h-[80vh] mx-[10vh] my-[10vh] shadow-2xl bg-blue-400'>
-        <section className='grid grid-cols-3'>
-          <div id='sideBar' className='rounded-md h-[73vh] m-5 bg-blue-300'>
-            <section className='grid grid-cols-1 gap-4'>
+      <main className='rounded-xl h-[80vh] mx-[30vh] my-[10vh] shadow-2xl bg-amber-950'>
+        <section className='grid grid-cols-3 gap-2 p-3'>
+          <div id='sideBar' className='rounded-md h-[75vh] overflow-auto bg-amber-100'>
+            <section className='h-[30vh] m-2 bg-amber-200'>
 
             </section>
+            <section className='grid grid-cols-1'>
+              {Sections.map(({ id, title, description, component }) => (
+                <span className='p-3 w-auto cursor-pointer hover:bg-amber-200 bg-amber-100 '>
+                  <h4 className='font-semibold'>{title}</h4>
+                  <span className='text-xs italic'>{description}</span>
+                </span>
+              ))}
+            </section>
           </div>
-          <div id='content' className='rounded-md col-span-2 h-[73vh] m-5 bg-blue-300'>
+          <div id='content' className='rounded-md col-span-2 h-[75vh] bg-amber-100'>
 
           </div>
         </section>
--- src/data/Sections.ts
+++ src/data/Sections.ts
@@ -1,9 +1,12 @@
 interface sections {
+    id: number,
     title: string,
     description: string,
     component: string
 }
 
 export const Sections: sections[] = [
-    { title: '', description: '', component: '' }
+    { id: 1, title: 'Intro', description: 'A little description about me', component: '' },
+    { id: 2, title: 'Test', description: 'A little description about me', component: '' },
+    { id: 3, title: 'Test', description: 'A little description about me', component: '' },
 ]
